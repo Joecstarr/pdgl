4f83fc1462839e5aa37b1c7e7ec90397adb0cdc9
Joe Starr <joe@joe-starr.com>
2026-02-09 20:34:24 -0600

    Improve: Add Doxygen comments for pdgl_defs

--- source/defs/pdgl_defs.h
+++ source/defs/pdgl_defs.h
@@ -1,10 +1,10 @@
-/*!
- *  @file pdgl_defs.h
+/**
+ *  \file pdgl_defs.h
  *
- *  @brief  Common definitions for the PDGL.
+ *  \brief Common definitions for the PDGL.
  *
  *
- *  @author    Joe Starr
+ *  \author Joe Starr
  *
  */
 
@@ -20,7 +20,7 @@
 #ifdef _WIN32
 #ifndef _CRT_SECURE_NO_WARNINGS
 
-/*! For portability we're using the old style "unsafe" string interfaces. We can suppress warning on
+/** For portability we're using the old style "unsafe" string interfaces. We can suppress warning on
  * windows. */
 #define _CRT_SECURE_NO_WARNINGS    (1u)
 #else
@@ -32,7 +32,7 @@
 #endif
 #ifndef _CRT_DEPRECATE_TEXT
 
-/*! For portability we're using the old style "unsafe" string interfaces. We can suppress warning on
+/** For portability we're using the old style "unsafe" string interfaces. We can suppress warning on
  * windows. */
 #define _CRT_DEPRECATE_TEXT    (1u)
 #else
@@ -48,12 +48,24 @@
 #ifndef STATIC_INLINE
 #if defined(__GNUC__) || defined(__cplusplus) || \
     (defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901L))
+
+/**
+ * \brief standard static inline macro
+ */
 #define STATIC_INLINE    static inline
 #else
+
+/**
+ * \brief standard static inline macro
+ */
 #define STATIC_INLINE    static
 #endif
 #endif /* STATIC_INLINE */
 
+
+/**
+ * \brief Static inline macro for a uint8_t function
+ */
 #define STATIC_INLINE_UINT8    STATIC_INLINE uint8_t
 /******************************************************************************/
 /****************************** Includes **************************************/
@@ -67,8 +79,23 @@
 /****************************** Defines **************************************/
 /******************************************************************************/
 
+/**
+ * \brief Define the maximum size of a string in the PDGL
+ */
 #define DEFS_PDGL_MAX_STRING_SIZE    (3000)
+
+/**
+ * \brief Define the maximum size of a production name in the PDGL
+ */
 #define DEFS_PDGL_MAX_NAME_SIZE      (200)
+
+/**
+ * \brief Define the name of the required entry symbol in the PDGL
+ */
 #define DEFS_PDGL_ENTRY_SYMBOL       ("entry")
+
+/**
+ * \brief Define a macro for match result from the strcmp function
+ */
 #define DEFS_PDGL_STRING_MATCH       (0x0u)
 #endif /* end UTIL_PDGL_DEFS_H */
